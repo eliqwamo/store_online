fc3c5217fb060589853c99169291fc26faa763af
eliqwamo <eli@qwamo.com>
2022-04-25 13:08:16 +0300

    25042022

--- accounts.js
+++ accounts.js
@@ -1,22 +1,56 @@
 import express from 'express';
+import tom from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
+
+let instruments = [];
+let students = [];
 
 router.post('/login', (req,res) => {
 
-    //1
     const fn = req.body.name;
     const ln = req.body.lastname;
 
-    //2
-    const {name,lastname} = req.body;
+    //const {name,lastname} = req.body;
 
     return res.status(200).json({
-        message: `Hello ${name} ${lastname}`
+        message: `Hello ${fn} ${ln}`
     })
 })
 
-router.get('/register', (req,res) => {
 
+router.post('/register', async(req,res) => {
+    const {fname, lname,email,password} = req.body;
+    const isStudent = students.filter(x => x.email == email);
+    if(isStudent > 0){
+        return res.status(200).json({
+            //NO
+        });
+    } else {
+
+        const hash = await tom.hash(password, 10);
+
+        const student = {
+            name: fname + ' ' + lname,
+            email: email,
+            password: hash
+        }
+        students.push(student);
+        return res.status(200).json({
+            students: students
+        });
+    }
+
+    
+
+})
+
+router.post('/addInstrument', async(req,res) => {
+    const instrument = req.body.instrument;
+    instruments.push(instrument);
+    return res.status(200).json({
+        instruments: instruments
+    });
 })
 
 //module.exports = router;
